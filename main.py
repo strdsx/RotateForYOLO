42b563985d3e0850e9fe58a8d6fc8253b66178c5
jaehyeon <bigjh64@gmail.com>
2019-10-30 19:53:56 +0900

    added augmentor

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import Augmentor
+import os
+import random
+import shutil
+
 from data_aug.data_aug import *
 from data_aug.bbox_util import *
 import cv2 
@@ -7,6 +12,67 @@ import matplotlib.pyplot as plt
 from multiprocessing import Process
 
 # Reference : https://github.com/Paperspace/DataAugmentationForObjectDetection
+# Reference : https://github.com/mdbloice/Augmentor
+
+def Augmentation_Operator(input_path, output_path, sample_num):
+    # Pipe Init.
+    ############################ Fix !!! ############################
+    p = Augmentor.Pipeline(input_path, output_path)
+    
+    p.random_erasing(probability=0.3, rectangle_area=0.10000001)
+    p.gaussian_distortion(probability=0.7, grid_width=3, grid_height=3, magnitude=3, corner="bell", method="in")
+    p.random_distortion(probability=0,2, grid_width=2, grid_height=2, magnitude=2)
+    p.histogram_equalisation(probability=0.5)
+    p.random_brightness(probability=0.8, min_factor=0.3, max_factor=0.8)
+
+    # Scale Transform
+    p.scale(probability=0.6, scale_factor=1.3)
+    p.scale(probability=0.3, scale_factor=1.5)
+
+
+    p.sample(sample_num)
+    ############################ Fix !!! ############################
+
+# Augmentation
+def augmentation(input_path):
+    aug_path = input_path + "_aug/" # <== Augmentation 이미지 저장 경로
+    output_path = "../" + aug_path
+    
+    # Number of image
+    sample_number = 1000000
+    Augmentation_Operator(input_path, aug_path, sample_number)
+
+    # Merge
+    imglist = os.listdir(aug_path)
+
+    cnt = 0
+
+    # Augmentation img list.
+    # Augmentation 완료 이후, 원본 이미지와 파일 이름 매칭 후,
+    # 해당 txt 파일 가져와서 merge
+    orig_folder = input_path.split("/")[-1]
+    for i in imglist:
+        tmp = i.split(orig_folder +"_original_")[1]
+        i_num = tmp.split(".")[0]
+
+        new_name = aug_path + str(cnt) + ".jpg"
+
+        # GaussianBlur
+        if cnt % 5 == 0:
+            img = cv2.imread(aug_path + i)
+            blur_img = cv2.GaussianBlur(img, (3, 3), 0)
+            cv2.imwrite(aug_path + i, blur_img)
+
+        # Rename augmentation image files.
+        shutil.move(aug_path + i, new_name)
+
+        # Get .txt files in the origin piap folder.
+        shutil.copy(input_path + "/" + i_num + ".txt", aug_path + str(cnt) + ".txt")
+
+        print(cnt, "/", sample_number)
+
+        cnt += 1
+
 
 # to TL RB
 def trans_bbox(bbox_info, origin_width, origin_height):
@@ -27,98 +93,97 @@ def trans_bbox(bbox_info, origin_width, origin_height):
 
     return return_list
 
-def main(image_folder, start, end):    
+def rotate_func(image_list, start, end):    
     save_path = image_folder
 
-    image_list = [x for x in os.listdir(image_folder) if x.split(".")[1] == "jpg"]
+    for im in image_list[start:end]:
+        img_name = image_folder + im
+        txt_name = image_folder + im.split(".")[0] + ".txt"
 
-    for cnt, im in enumerate(image_list):
-        if start < cnt < end:
-            img_name = image_folder + im
-            txt_name = image_folder + im.split(".")[0] + ".txt"
+        # Read image
+        img = cv2.imread(img_name) #OpenCV uses BGR channels
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        height, width = img.shape[:2]
 
-            # Read image
-            img = cv2.imread(img_name) #OpenCV uses BGR channels
+        # Read txt for YOLO training
+        with open(txt_name, "r") as f:
+            lines = f.readlines()
 
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            height, width = img.shape[:2]
+        if len(lines) > 0:
+            for i in range(len(lines)):
+                lines[i] = lines[i].split("\n")[0]
+                lines[i] = lines[i].split(" ")
 
-            # Read txt for YOLO training
-            with open(txt_name, "r") as f:
-                lines = f.readlines()
+            # transform bbox
+            bboxes = trans_bbox(lines, width, height)
+            bboxes = np.array(bboxes, dtype=np.float)
+            
+            try:
+                transforms = Sequence([RandomRotate(15)])
 
-            if len(lines) > 0:
-                
-                for i in range(len(lines)):
-                    lines[i] = lines[i].split("\n")[0]
-                    lines[i] = lines[i].split(" ")
+                # transformed image
+                trans_img, trans_bboxes = transforms(img, bboxes)
+                tH, tW = trans_img.shape[:2]
 
-                # transform bbox
-                bboxes = trans_bbox(lines, width, height)
-                bboxes = np.array(bboxes, dtype=np.float)
-                
-                try:
-                    transforms = Sequence([RandomRotate(15),
-                        RandomHorizontalFlip(1),
-                        RandomScale(0.2, diff=False)])
+                # init save path & save image, txt files
+                save_img = save_path + "rot_" + im
+                save_txt = save_path + "rot_" + im.split(".")[0] + ".txt"
 
-                    # transformed image
-                    trans_img, trans_bboxes = transforms(img, bboxes)
-                    tH, tW = trans_img.shape[:2]
+                trans_img = cv2.cvtColor(trans_img, cv2.COLOR_RGB2BGR)
+                cv2.imwrite(save_img, trans_img)
+                with open(save_txt, "w") as f:
+                    for i in trans_bboxes:
+                        x1 = int(i[0])
+                        y1 = int(i[1])
+                        x2 = int(i[2])
+                        y2 = int(i[3])
 
-                    # init save path & save image, txt files
-                    save_img = save_path + "rot_" + im
-                    save_txt = save_path + "rot_" + im.split(".")[0] + ".txt"
+                        # check
+                        '''
+                        # trans_img = cv2.rectangle(trans_img, (x1, y1), (x2, y2), (0,255,0), 1)
+                        crop_img = trans_img[y1:y2, x1:x2]
+                        cv2.imshow("crop", crop_img)
+                        cv2.waitKey()
+                        cv2.destroyAllWindows()
+                        '''
 
-                    cv2.imwrite(save_img, trans_img)
-                    with open(save_txt, "w") as f:
-                        for i in trans_bboxes:
-                            x1 = int(i[0])
-                            y1 = int(i[1])
-                            x2 = int(i[2])
-                            y2 = int(i[3])
+                        c_index = str(int(i[4])) # class index
 
-                            # check
-                            '''
-                            # trans_img = cv2.rectangle(trans_img, (x1, y1), (x2, y2), (0,255,0), 1)
-                            crop_img = trans_img[y1:y2, x1:x2]
-                            cv2.imshow("crop", crop_img)
-                            cv2.waitKey()
-                            cv2.destroyAllWindows()
-                            '''
+                        # to relative coordinate for YOLO training
+                        rcx = str(round(float(((x2 - x1) / 2 + x1) / tW), 6))
+                        rcy = str(round(float(((y2 - y1) / 2 + y1) / tH), 6))
+                        rw = str(round(float((x2-x1)/tW), 6))
+                        rh = str(round(float((y2-y1)/tH), 6))
 
-                            c_index = str(int(i[4])) # class index
-
-                            # to relative coordinate for YOLO training
-                            rcx = str(round(float(((x2 - x1) / 2 + x1) / tW), 6))
-                            rcy = str(round(float(((y2 - y1) / 2 + y1) / tH), 6))
-                            rw = str(round(float((x2-x1)/tW), 6))
-                            rh = str(round(float((y2-y1)/tH), 6))
-                            # print(rcx, rcy, rw, rh, c_index)
-                            yolo_txt = c_index + " " + rcx + " " + rcy + " " + rw + " " + rh + "\n"
-                            f.write(yolo_txt)
-                    # check
-                    '''
-                    # image show
-                    trans_img = cv2.resize(trans_img, dsize=(0,0),fx=4, fy=4)
-                    cv2.imshow("test", trans_img)
-                    cv2.waitKey()
-                    '''
-                except OSError as err:
-                    print("error => ", err)
+                        yolo_txt = c_index + " " + rcx + " " + rcy + " " + rw + " " + rh + "\n"
+                        f.write(yolo_txt)
+                # check
+                '''
+                # image show
+                trans_img = cv2.resize(trans_img, dsize=(0,0),fx=4, fy=4)
+                cv2.imshow("test", trans_img)
+                cv2.waitKey()
+                '''
+            except OSError as err:
+                print("error => ", err)
 
 
 if __name__ == '__main__':
-    image_folder = "/home/jaehyeon/data/Drowner/drowner_train/"
-    image_list = [x for x in os.listdir(image_folder) if x.endswith(".jpg")]
+    # Augmentor
+    origin_folder = "/home/cvserver3/project/JH/Dataset/piap/LPC"
+    augmentation(origin_folder)
+
+    # Rotation
+    aug_folder = "/home/cvserver3/project/JH/Dataset/piap/LPC_aug/"
+    image_list = [x for x in os.listdir(aug_folder) if x.endswith(".jpg")]
     image_num = len(image_list)
     split_num = 4
     proc_list = []
 
     for i in range(split_num):
-        start = int(image_num * i / 4)
-        end = int(image_num * (i + 1) / 4)
-        proc = Process(target=main, args=(image_folder, start, end,))
+        start = int(image_num * i / split_num)
+        end = int(image_num * (i + 1) / split_num)
+        proc = Process(target=rotate_func, args=(image_list, start, end,))
         proc_list.append(proc)
         proc.start()
 
